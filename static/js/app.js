aea0a223c0510b96ba93038cbb31e41bf5fa0ae5
gakunkel <gkunkel@ucsd.edu>
2021-03-24 16:04:42 -0700

    Beginning table population

--- static/js/app.js
+++ static/js/app.js
@@ -8,10 +8,6 @@ var button = d3.select("#filter-btn");
 // Reference html file's tbody tag for inserting data
 var tbody = d3.select("tbody");
 
-// tableData.forEach(function(report) {
-//     var row = tbody.append("tr");
-// });
-
 // Event handlers for button clicks
 button.on("click", runEnter);
 form.on("submit", runEnter);
@@ -42,9 +38,12 @@ function runEnter() {
     var filteredState = filteredData.map(report => report.state);
     var filteredCountry = filteredData.map(report => report.country);
     var filteredShape = filteredData.map(report => report.shape);
-    var filteredDuration = filteredData.map(report => report.duration);
+    var filteredDuration = filteredData.map(report => report.durationMinutes);
     var filteredComments = filteredData.map(report => report.comments);
 
-
+    // Appending the filtered data elements to the tbody
+    filteredData.forEach(function(report) {
+        var row = tbody.append("tr");
+    })
 };
 
