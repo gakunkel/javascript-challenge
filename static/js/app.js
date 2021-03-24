39d5f4a0c5f188f981bc05ecab9cdcb3cf311ef8
gakunkel <gkunkel@ucsd.edu>
2021-03-24 15:12:35 -0700

    Initial D3 selections (form, button input)

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,35 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Need to select interactive html tags
+var form = d3.select("form");
+var button = d3.select("#filter-btn");
+
+// Reference html file's tbody tag for inserting data
+var tbody = d3.select("tbody");
+
+// tableData.forEach(function(report) {
+//     var row = tbody.append("tr");
+// });
+
+// Event handlers for button clicks
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+function runEnter() {
+
+    // Preventing the page from reloading
+    d3.event.preventDefault();
+
+    // Selecting the user's input date
+    var inputElement = d3.select("#datetime");
+
+    var inputValue = inputElement.property("value");
+
+    // Don't tell the aliens we keep our user input there!!
+    console.log("User Date Input: " + inputValue);
+
+
+
+};
+
